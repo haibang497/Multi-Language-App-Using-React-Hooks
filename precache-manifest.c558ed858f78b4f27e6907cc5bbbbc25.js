9611ce440b807af64287071d4c47d1623440fc16
haibang497 <haibang497@gmail.com>
2020-09-17 14:12:29 +0700

    Updates

---
+++ precache-manifest.c558ed858f78b4f27e6907cc5bbbbc25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31be75be0784dbc073ac024c3d3ade58",
+    "url": "/Multi-Language-App-Using-React-Hooks/index.html"
+  },
+  {
+    "revision": "9155938dfec6d163320b",
+    "url": "/Multi-Language-App-Using-React-Hooks/static/css/main.9a50f153.chunk.css"
+  },
+  {
+    "revision": "e7a6e570e2681755988d",
+    "url": "/Multi-Language-App-Using-React-Hooks/static/js/2.17766f4e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Multi-Language-App-Using-React-Hooks/static/js/2.17766f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9155938dfec6d163320b",
+    "url": "/Multi-Language-App-Using-React-Hooks/static/js/main.654f6c35.chunk.js"
+  },
+  {
+    "revision": "67ac6c4d78355cef4932",
+    "url": "/Multi-Language-App-Using-React-Hooks/static/js/runtime-main.e1b7f43c.js"
+  }
+]);
